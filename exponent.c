2097991fc30329d45b07df2156b917490fc8fe48
AdiSuyash <adityasuyashgupta@gmail.com>
2023-01-18 21:55:37 +0530

    exponent

---
+++ exponent.c
@@ -0,0 +1,21 @@
+// Power of Any Number
+
+#include<stdio.h>
+int a,n;
+int power(int x, int y){
+    if (y==1)
+    {
+        return x;
+    }
+    else{
+        return x*power(x,y-1);
+    }
+    
+}   
+int main()
+{
+    printf("Enter Base & Exponent: ");
+    scanf("%d %d",&a,&n);
+    int o=power(a,n);
+    printf("The value is %d",o);
+}
