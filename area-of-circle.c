623fed59d2d75fa393b38849647a3fa680bfe708
AdiSuyash <adityasuyashgupta@gmail.com>
2023-02-17 07:54:48 +0530

    Area & Circumference of Circle

--- area-of-circle.c
+++ area-of-circle.c
@@ -4,16 +4,16 @@
 
 int main()
 {
-    int radius;
+    float radius;
     float area, circumference;
 
     printf("Enter the radius of the circle :");
-    scanf("%d", &radius);
+    scanf("%f", &radius);
 
     area = 3.14 * radius * radius;
     circumference = 2 * 3.14 * radius;
 
-    printf("The Area of Circle is :%0.2f", &area);
-    printf("The Circumference of Circle is :%0.2f", &circumference);
+    printf("The Area of Circle is :%0.2f", area);
+    printf("\nThe Circumference of Circle is :%0.2f", circumference);
     return 0;
 }
