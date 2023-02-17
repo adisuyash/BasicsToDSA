34d60ce4a0b7ab8f0450be6dbe651ac5dd2bbf0a
AdiSuyash <adityasuyashgupta@gmail.com>
2023-02-17 08:10:50 +0530

    commits

--- output+value of printf.c
+++ output+value of printf.c
@@ -5,6 +5,6 @@ int main()
     int i;
     i = printf("Adi\n");
     // 'printf' firstly prints whatever written in "__" & has a value assigned to it which is = 'no. of characters it print'
-    printf("%d", i);
+    printf("%d", i); 
     return 0;
 }
