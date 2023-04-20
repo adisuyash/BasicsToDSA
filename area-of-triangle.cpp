06b8c329abff6ac1afa41376c81b1bc65954c0b8
AdiSuyash <adityasuyashgupta@gmail.com>
2023-04-20 19:52:39 +0530

    (base*height)/2

---
+++ area-of-triangle.cpp
@@ -0,0 +1,20 @@
+// 
+
+#include<iostream>
+using namespace std;
+    
+int main()
+{
+    float area,base,height;
+
+    cout<<"Enter base :";
+    cin>>base;
+    cout<<"Enter height :";
+    cin>>height;
+
+    area=(base*height)/2;
+
+    cout<<"The area of triangle is :"<<area;
+    
+    return 0;
+}
