89b57bbb6fcb16655b25b4834d1432ad0d73d83e
AdiSuyash <adityasuyashgupta@gmail.com>
2023-04-29 07:31:31 +0530

    factorial added

--- sum-n-numbers.cpp
+++ sum-n-numbers.cpp
@@ -5,13 +5,25 @@ using namespace std;
 
 int main()
 {
-    int n, sum = 0;
+    int n, sum = 0, fact = 1;
+
     cout << "Enter n :";
     cin >> n;
+
+    // Sum of Natural Numbers upto n
     for (int i = 1; i <= n; i++)
     {
         sum += i;
     }
+
+    // Factorial of n
+    for (int j = 1; j <= n; j++)
+    {
+        fact *= i;
+    }
     cout << "The Sum of " << n << " natural numbers is " << sum;
+    cout << "The Factorial of " << n << " is " << fact;
+
+
     return 0;
 }
